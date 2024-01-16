3e79d487653df81ad84d1d59aa78d831a7ffaeff
EnricoDG97 <enrico.delgaudio97@gmail.com>
2024-01-16 20:30:04 +0100

    feat: scaffolding, created controller, model, migration with trains table and each column types

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container mt-5 text-center">
+    <h1>LISTA TRENI</h1>
+    <h3>Treni della terza settimana 2024</h3>
+    <div class="container">
+        qui tutti i treni
+    </div>
+    <hr>
+    <h3>Treni in partenza in data odierna:</h3>
+    <div class="container">
+        qui i treni in partenza data odierna
+    </div>
+</div>
+@endsection
